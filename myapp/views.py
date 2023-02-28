51a1cee914f86c6aec0dbde352772c439c61050b
Sebastian009w <ia3266505@gmail.com>
2023-02-28 00:26:03 -0500

    hello world en django

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def hello_world(request):
+    """.REPONSE"""
+    return HttpResponse("<h1>Hello world</h1>")
+
+def about(request):
+    """.About"""
+    return HttpResponse("<h2>About</h2>")
